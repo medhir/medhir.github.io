9e89b79ac8f55485aec423997fdeaf902a432950
Medhir Bhargava <medhirbhargava@gmail.com>
2015-06-13 16:44:58 -0700

    Finished first circle element

--- public/js/abstraction.js
+++ public/js/abstraction.js
@@ -1,43 +1,30 @@
 //set element variables
 var titleWidth = parseInt(d3.select('#titleContainer').style('width'), 10),
     titleHeight = titleWidth,
-    radius = titleWidth/2.5, 
-    lineWidth = titleWidth / 5,
-    lineCount = 180,
+    radius = titleWidth/3, 
+    lineWidth = titleWidth / 8,
+    lineCount = 1000,
     lineHeight = 1,
-    speed = 0.075
     color = "white";
 
-var svgContainer = d3.select('#titleContainer')
-                     .append('svg')
-                     .attr({
-                        width: titleWidth,
-                        height: titleHeight
-                     });
-
-var circle = d3.select('#titleContainer').selectAll("span")
+d3.select('#titleContainer').selectAll("div")
              .data(d3.range(lineCount))
-             .enter()
-             .append("span")
-             .attr({
-              className: function(i) {
-                if(i%2 === 0) return "rotate-right";
-                return "rotate-left"
-              }, 
-              style({
-                top: function(i) {
-                  return Math.round(titleHeight/2 + radius * Math.sin(i)) - lineWidth/2;
-                }, 
-                left: function(i) {
-                  return Math.round(titleWidth/2 + radius * Math.sin(i))
-                }, 
-
+             .enter().append("div")
+             .attr("class", function(i) {
+                  if(i%2 === 0) return "rotate-right";
+                  else return "rotate-left";
+              })
+              .style('top', function(i) { return (Math.round(titleHeight/2 + radius * Math.sin(i))) + 'px'; })
+              .style('left', function(i) { return (Math.round(titleWidth/2 + radius * Math.cos(i)) - lineWidth/2) + 'px'; })
+              .style({ 
+                'background-color' : color, 
+                'height': lineHeight + 'px',
+                'width': lineWidth + 'px',
+                'position': 'absolute'
               });
 
-console.dir(circle);
+d3.select('titleContainer').append('h2')
+  .
 
-d3.timer(function(elapsed) {
-  d3.selectAll('rect')
-      .attr("transform", function(t) { return "rotate(" + (t * 360 + elapsed * speed) + ")"; });
-});
+console.dir(circle);
 

b5efc60ceee613cf1d2a0cd5f88ff6e113075505
Medhir Bhargava <medhirbhargava@gmail.com>
2015-06-14 18:20:38 -0700

    Animation change

--- public/js/abstraction.js
+++ public/js/abstraction.js
@@ -7,21 +7,21 @@ var titleWidth = parseInt(d3.select('#titleContainer').style('width'), 10),
     lineHeight = 1,
     color = "white";
 
-d3.select('#titleContainer').selectAll("div")
-             .data(d3.range(lineCount))
-             .enter().append("div")
-             .attr("class", function(i) {
-                  if(i%2 === 0) return "rotate-right";
-                  else return "rotate-left";
-              })
-              .style('top', function(i) { return (Math.round(titleHeight/2 + radius * Math.sin(i))) + 'px'; })
-              .style('left', function(i) { return (Math.round(titleWidth/2 + radius * Math.cos(i)) - lineWidth/2) + 'px'; })
-              .style({ 
-                'background-color' : color, 
-                'height': lineHeight + 'px',
-                'width': lineWidth + 'px',
-                'position': 'absolute'
-              });
+// d3.select('#titleContainer').selectAll("div")
+//              .data(d3.range(lineCount))
+//              .enter().append("div")
+//              .attr("class", function(i) {
+//                   if(i%2 === 0) return "rotate-right";
+//                   else return "rotate-left";
+//               })
+//               .style('top', function(i) { return (Math.round(titleHeight/2 + radius * Math.sin(i))) + 'px'; })
+//               .style('left', function(i) { return (Math.round(titleWidth/2 + radius * Math.cos(i)) - lineWidth/2) + 'px'; })
+//               .style({ 
+//                 'background-color' : color, 
+//                 'height': lineHeight + 'px',
+//                 'width': lineWidth + 'px',
+//                 'position': 'absolute'
+//               });
 
 d3.select('#titleContainer').append('h4')
   .text('exploring abstraction')
@@ -37,3 +37,29 @@ d3.select('#titleContainer h4')
     'padding-top' : (titleHeight/2 - abstractionHeight) + 'px', 
     'padding-bottom' : (titleHeight/2 - abstractionHeight) + 'px'
   });
+
+var appendLine = function(i) {
+  var x = (Math.round(titleWidth/2 + radius * Math.cos(i)) - lineWidth/2) + 'px'; 
+      y = (Math.round(titleHeight/2 + radius * Math.sin(i))) + 'px'; 
+  d3.select('#titleContainer').append("div")
+                .attr("class", "rotate-right")
+                .style('top', y)
+                .style('left', x)
+                .style({ 
+                  'background-color' : color, 
+                  'height': lineHeight + 'px',
+                  'width': lineWidth + 'px',
+                  'position': 'absolute'
+                });
+};
+
+var generateTitle = function() {
+  var count = 0; 
+  var interval = setInterval(function(){
+    appendLine(count);
+    count++;
+    if(count === 360) clearInterval(interval);
+  },1);
+};
+
+generateTitle();
